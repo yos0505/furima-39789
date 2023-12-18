f763bfbe2144c075aafec708e019ad8fe70c4bfc
yos0505 <yos2209@icloud.com>
2023-12-18 15:19:12 +0900

    新規登録時のパラメーターの設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,4 +9,8 @@ class ApplicationController < ActionController::Base
       username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"] 
     end
   end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :email, :password,  :last_name_kanji, :first_name_kanji, :last_name_kana, :first_name_kana, :birthday])
+  end
 end
