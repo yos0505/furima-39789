41d3a6fca01f6d9eed31828f8b306264f2afe801
yos0505 <yos2209@icloud.com>
2023-12-26 21:41:09 +0900

    確認用パスワードの修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,7 +3,7 @@ FactoryBot.define do
     nickname              {Faker::Name.initials(number: 2)}
     email                 {Faker::Internet.email}
     password              {Faker::Internet.password(min_length: 6) + '1a'}
-    encrypted_password    {password}
+    password_confirmation {password}
     last_name_kanji       {'山田' }
     first_name_kanji      {'太郎'}
     last_name_kana        {'ヤマダ'}
