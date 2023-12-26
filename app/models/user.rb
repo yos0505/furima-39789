755d8bb8ebad0e02294a62c58980520e17e9af40
yos0505 <yos2209@icloud.com>
2023-12-26 20:05:41 +0900

    emailバリデーションと、ヘッダーコメントアウトの削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
          validates :nickname, presence: true
-         validates :email, presence: true
          validates :last_name_kanji, presence: true
          validates :first_name_kanji, presence: true
          validates :last_name_kana, presence: true
