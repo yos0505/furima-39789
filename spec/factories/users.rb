8c9422b2839f9ee849494b14346d57cddd1af728
yos0505 <yos2209@icloud.com>
2023-12-26 20:36:28 +0900

    FactoryBotでのパスワードの設定

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     nickname              {Faker::Name.initials(number: 2)}
     email                 {Faker::Internet.email}
-    password              {Faker::Internet.password(min_length: 6)}
+    password              {Faker::Internet.password(min_length: 6) + '1a'}
     encrypted_password    {password}
     last_name_kanji       {'山田' }
     first_name_kanji      {'太郎'}
