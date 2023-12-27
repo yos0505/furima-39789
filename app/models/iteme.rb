2c378f1854bcdf1425fa99708c26d3f4c60a5cd2
yos0505 <yos2209@icloud.com>
2023-12-27 21:33:55 +0900

    itemsモデルのアソシエーションの設定

--- app/models/iteme.rb
+++ app/models/iteme.rb
@@ -1,4 +1,6 @@
 class Iteme < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category, :condition, :shipping_charge, :shipping_from, :shipping_day
 end
