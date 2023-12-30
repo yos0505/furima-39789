000b999c5010fe7cf6e5c9cd10dff55b40716584
yos0505 <yos2209@icloud.com>
2023-12-30 13:31:42 +0900

    itemモデルのファクトリーボットの定義

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,15 @@
 FactoryBot.define do
   factory :item do
-    
+    name                 { 'カレンダー' }
+    description          { '2024年度のカレンダーです' }
+    category_id          { 2 }
+    condition_id         { 2 }
+    shipping_charge_id   { 2 }
+    shipping_from_id     { 2 }
+    shipping_day_id      { 2 }
+    price                { 2000 }
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/28241370_s.jpg'), filename: '28241370_s.jpg')
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
-
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+  before do
+    @item = FactoryBot.build(:item)
+  end
+
+  describe '商品出品機能' do
+    context '商品出品できるとき' do
+      it 'すべての入力項目が存在すれば出品できる' do
+      expect(@item).to be_valid
+    end
+    it "商品名が40文字以下であれば登録できる" do
+      @user.password = "000aaa"
+      @user.password_confirmation = "000aaa"
+      expect(@user).to be_valid
+    end
+    it "商品の説明が1000文字以下であれば登録できる" do
+    end
+    it "商品画像があれば登録できる" do
+    end
