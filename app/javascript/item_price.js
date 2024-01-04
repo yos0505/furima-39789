97ed7de0bc57d0aae0612bc655ed1057350273ad
yos0505 <yos2209@icloud.com>
2024-01-04 20:30:42 +0900

    販売手数料、利益計算の実装

---
+++ app/javascript/item_price.js
@@ -0,0 +1,18 @@
+window.addEventListener('turbo:load', () => {
+  console.log("OK");
+});
+const priceInput = document.getElementById("item-price");
+console.log(priceInput);
+
+priceInput.addEventListener("input", () => {
+  const inputValue = priceInput.value;
+  console.log(inputValue);
+
+const addTaxDom = document.getElementById("add-tax-price");
+const commission = Math.floor(inputValue * 0.1);
+addTaxDom.innerHTML = commission
+
+const profitDom = document.getElementById("profit");
+profitDom.innerHTML = Math.floor(inputValue - commission);
+})
+
