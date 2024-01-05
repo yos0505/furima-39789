a72967acafb8ecb7160c2417132569a00ea641a1
yos0505 <yos2209@icloud.com>
2024-01-05 11:35:26 +0900

    コントローラーで現在使用しないコードのコメントアウト

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new], unless: :user_signed_in?
 
   def index
-    @items = Item.order("created_at DESC")
+   # @items = Item.order("created_at DESC")
 
   end
 
