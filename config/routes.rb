7e649f8473e253240eb3fe384fef22a42c7374b2
yos0505 <yos2209@icloud.com>
2024-01-08 17:02:47 +0900

    createアクションの定義とルーテイングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
   resources :items, only: [:index, :new, :create, :show, :edit, :update, :destroy]
-  root to: "orders#index"
-  resources :orders, only: :index
+  resources :orders, only: [:index, :new, :create]
 end
