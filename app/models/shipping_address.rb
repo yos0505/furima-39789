e6b211842d0c52d22b17976cf437419569a25ceb
yos0505 <yos2209@icloud.com>
2024-01-09 19:04:47 +0900

    バリデーションの修正

--- app/models/shipping_address.rb
+++ app/models/shipping_address.rb
@@ -1,8 +1,8 @@
 class ShippingAddress < ApplicationRecord
   belongs_to :order
   with_options presence: true do
-    validates :post_cord, format: {/\A\d{3}[-]\d{4}\z/} 
-    validates :phone_number, format: {/\A\d{10,11}\z/}
+    validates :post_cord, format: { with: /\A\d{3}[-]\d{4}\z/ } 
+    validates :phone_number, format: { with: /\A\d{10,11}\z/ }
   end
-    validates :shipping_from_id, :city, :street_address, presence: true
+  validates :shipping_from_id, :city, :street_address, presence: true
 end
