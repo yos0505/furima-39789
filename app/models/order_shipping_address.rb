0bb62da71d4e4cfe471de1ea6b8e29ead640d9c1
yos0505 <yos2209@icloud.com>
2024-01-10 22:11:49 +0900

    OrderShippingAddressモデルにカラムを指定

--- app/models/order_shipping_address.rb
+++ app/models/order_shipping_address.rb
@@ -1,2 +1,4 @@
 class OrderShippingAddress
+  include ActiveModel::Model
+  attr_accessor :post_cord, :shipping_from_id, :city, :street_address, :building_name, :phone_number, :order_id, :user_id, :item_id
 end
