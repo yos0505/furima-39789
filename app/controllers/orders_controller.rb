2e1b0c948730ac67ff43e86fc826f4463ab2c89f
yos0505 <yos2209@icloud.com>
2024-01-11 20:06:48 +0900

    orders_controllerの修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,26 +2,21 @@ class OrdersController < ApplicationController
   before_action :authenticate_user!, except: :index
 
   def index
-    @items = Item.all
-    @order = Order.new
+    @order_shipping_address = OrderShippingAddress.new
   end
 
   def create
-    @order = Order.new(order_params)
-    ShippingAddress.create(shipping_address_params)
-    if @order.valid?
-      @order.save
-      return redirect_to root_path
+    @order_shipping_address = OrderShippingAddress.new(order_params)
+    if  @order_shipping_address.valid?
+       @order_shipping_address.save
+       redirect_to root_path
     else
     render :create, status: :unprocessable_entity
     end
   end
 
   def order_params
-    params.require(:order).permit(:user_id, :item_id).merge(user_id: current_user.id)
+    params.require(:order_shipping_address).permit(:post_cord, :shipping_from_id, :city, :street_address, :phone_number, :user_id, :item_id).merge(user_id: current_user.id)
   end
 
-  def shipping_address_params
-    params.require(:order).permit(shipping_address: [:post_cord, :shipping_from_id, :city, :street_address, :building_name, :phone_number]).fetch(:order, {}).fetch(:shipping_address, {}).merge(order_id: @order.id)
-  end
 end
