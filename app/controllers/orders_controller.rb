d1a1f5e575df04f8ab72743e505e3b860b2c4b77
yos0505 <yos2209@icloud.com>
2024-01-15 21:17:35 +0900

    エラーメッセージの表示の実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -8,12 +8,13 @@ class OrdersController < ApplicationController
   end
 
   def create
+    @item = Item.find(params[:item_id])
     @order_shipping_address = OrderShippingAddress.new(order_params)
     if  @order_shipping_address.valid?
        @order_shipping_address.save
        redirect_to root_path
     else
-       render :create, status: :unprocessable_entity
+       render :index, status: :unprocessable_entity
     end
   end
 
