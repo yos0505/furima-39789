91218f6b941ff4497640ab69e481a13632dc9572
yos0505 <yos2209@icloud.com>
2024-01-15 18:26:03 +0900

    ビュー、コントローラー、ルーティングjの修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,6 +2,8 @@ class OrdersController < ApplicationController
   before_action :authenticate_user!, except: :index
 
   def index
+    @item = Item.find(params[:item_id])
+    @order = Order.new
     @order_shipping_address = OrderShippingAddress.new
   end
 
@@ -11,12 +13,14 @@ class OrdersController < ApplicationController
        @order_shipping_address.save
        redirect_to root_path
     else
-    render :create, status: :unprocessable_entity
+       render :create, status: :unprocessable_entity
     end
   end
 
+  private
+
   def order_params
-    params.require(:order_shipping_address).permit(:post_cord, :shipping_from_id, :city, :street_address, :phone_number, :user_id, :item_id).merge(user_id: current_user.id)
+    params.require(:order_shipping_address).permit(:post_cord, :shipping_from_id, :city, :street_address, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
   end
 
 end
--- app/models/order_shipping_address.rb
+++ app/models/order_shipping_address.rb
@@ -3,13 +3,15 @@ class OrderShippingAddress
   attr_accessor :post_cord, :shipping_from_id, :city, :street_address, :building_name, :phone_number, :order_id, :user_id, :item_id
 
   with_options presence: true do
-    validates :user_id, presence: true
-    validates :item_id, presence: true
+    validates :user_id
+    validates :item_id
     validates :post_cord, format: { with: /\A\d{3}[-]\d{4}\z/ } 
     validates :phone_number, format: { with: /\A\d{10,11}\z/ }
     validates :shipping_from_id, :city, :street_address
   end
 
   def save
-    order = Order.create(user_id: user_id, item_id,: item_id)
+    order = Order.create(user_id: user_id, item_id: item_id)
+    ShippingAddress.create(post_cord: post_cord, shipping_from_id: shipping_from_id, city: city, street_address: street_address, building_name: building_name, phone_number: phone_number, order_id: order_id)
+  end
 end
