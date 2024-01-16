ad56d97dd02af976e4c5c495370546937972ac4c
yos0505 <yos2209@icloud.com>
2024-01-16 14:44:20 +0900

    テストコードの実装と、売却済み商品の表示の実装

--- spec/factories/order_shipping_addresses.rb
+++ spec/factories/order_shipping_addresses.rb
@@ -6,5 +6,8 @@ FactoryBot.define do
     street_address    { '青山' }
     building_name     { '' }
     phone_number      { '09012345678' }
+    order_id          { 1 }
+    user_id           { 1 }
+    item_id           { 1 }
   end
 end
--- spec/models/order_shipping_address_spec.rb
+++ spec/models/order_shipping_address_spec.rb
@@ -1,5 +1,59 @@
 require 'rails_helper'
 
 RSpec.describe OrderShippingAddress, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    user = FactoryBot.create(:user)
+    @order_shipping_address = FactoryBot.build(:order_shipping_address, user_id: user.id)
+  end
+
+  describe '購入者情報の保存' do
+    context '内容に問題ない場合' do
+      it 'すべての値が正しく入力されていれば保存できること' do
+        expect(@order_shipping_address).to be_valid
+      end
+      it 'building_nameは空でも保存できること' do
+        @order_shipping_address.building_name = ''
+        expect(@order_shipping_address).to be_valid
+      end
+    end
+
+    context '内容に問題がある場合' do
+      it 'post_cordが空だと保存できないこと' do
+        @order_shipping_address.post_cord = ''
+        @order_shipping_address.valid?
+        expect(@order_shipping_address.errors.full_messages).to include("Post cord can't be blank")
+      end
+      it 'post_cordが半角のハイフンを含んだ正しい形式でないと保存できないこと' do
+        @order_shipping_address.post_cord = ''
+        @order_shipping_address.valid?
+        expect(@order_shipping_address.errors.full_messages).to include("Post cord is invalid")
+      end
+      it 'shipping_from_idを選択していないと保存できないこと' do
+        @order_shipping_address.shipping_from_id = '1'
+        @order_shipping_address.valid?
+        expect(@order_shipping_address.errors.full_messages).to include("Shipping from can't be blank")
+      end
+      it 'cityが空だと保存できないこと' do
+        @order_shipping_address.city = ''
+        @order_shipping_address.valid?
+        expect(@order_shipping_address.errors.full_messages).to include("City can't be blank")
+      end
+      it 'street_addressが空だと保存できないこと' do
+        @order_shipping_address.street_address = ''
+        @order_shipping_address.valid?
+        expect(@order_shipping_address.errors.full_messages).to include("Street address can't be blank")
+      end
+      it 'phone_numberが空だと保存できないこと' do
+        @order_shipping_address.phone_number = ''
+        @order_shipping_address.valid?
+        expect(@order_shipping_address.errors.full_messages).to include("Phone number can't be blank")
+      end
+      it 'phone_numberが10桁以上11桁以内の半角数値でないと保存できないこと' do
+        @order_shipping_address.phone_number = '１23456789'
+        @order_shipping_address.valid?
+        expect(@order_shipping_address.errors.full_messages).to include("Phone number is invalid")
+      end
+    end
+  end
 end
+
