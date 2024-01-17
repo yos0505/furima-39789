f11e4798738ef0af8144c87e0f74081d2497d9cb
yos0505 <yos2209@icloud.com>
2024-01-17 20:45:33 +0900

    カード情報入力フォームの作成

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,5 +1,13 @@
 const pay = () => {
   const payjp = Payjp('pk_test_22a5df0a816dbb03f5c7573a')
+  const elements = payjp.elements();
+  const numberElement = elements.create('cardNumber');
+  const expiryElement = elements.create('cardExpiry');
+  const cvcElement = elements.create('cardCvc');
+
+  numberElement.mount('#number-form');
+  expiryElement.mount('#expiry-form');
+  cvcElement.mount('#cvc-form');
   const form = document.getElementById('charge-form')
   form.addEventListener("submit", (e) => {
     console.log("フォーム送信時にイベント発火")
