690a44648eb04e3e97418bbae97ea5903e2cbb0f
yos0505 <yos2209@icloud.com>
2024-01-17 20:57:09 +0900

    トークンの値をフォームに含める実装

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -15,7 +15,10 @@ const pay = () => {
       if (response.error) {
       } else {
         const token = response.id;
-        console.log(token)
+        const renderDom = document.getElementById("charge-form");
+        const tokenObj = `<input value=${token} name='token'>`;
+        renderDom.insertAdjacentHTML("beforeend", tokenObj);
+        debugger;
       }
     });
     e.preventDefault();
