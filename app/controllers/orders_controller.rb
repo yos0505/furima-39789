a96d2fdf96f6306b527a00d6117f3eb9a2708ca0
yos0505 <yos2209@icloud.com>
2024-01-18 21:08:34 +0900

    コントローラーのリファクタリング

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -14,12 +14,7 @@ class OrdersController < ApplicationController
     @item = Item.find(params[:item_id])
     @order_shipping_address = OrderShippingAddress.new(order_params)
     if  @order_shipping_address.valid?
-      Payjp.api_key = "sk_test_f9f13abea01fb57d94e1acf3" 
-      Payjp::Charge.create(
-        amount: @item.price,  # 商品の値段
-        card: order_params[:token],    # カードトークン
-        currency: 'jpy'                 # 通貨の種類（日本円）
-      )
+       pay_item
        @order_shipping_address.save
        redirect_to root_path
     else
@@ -32,4 +27,13 @@ class OrdersController < ApplicationController
   def order_params
     params.require(:order_shipping_address).permit(:post_cord, :shipping_from_id, :city, :street_address, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
   end
+
+  def pay_item
+    Payjp.api_key = "sk_test_f9f13abea01fb57d94e1acf3" 
+    Payjp::Charge.create(
+      amount: @item.price,  # 商品の値段
+      card: order_params[:token],    # カードトークン
+      currency: 'jpy'                 # 通貨の種類（日本円）
+     )
+    end
 end
