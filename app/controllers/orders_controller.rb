a0a198b553c71b28b87024434e48088a9fbd428a
yos0505 <yos2209@icloud.com>
2024-01-18 21:03:42 +0900

    コントローラー　商品の値段の修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -14,6 +14,12 @@ class OrdersController < ApplicationController
     @item = Item.find(params[:item_id])
     @order_shipping_address = OrderShippingAddress.new(order_params)
     if  @order_shipping_address.valid?
+      Payjp.api_key = "sk_test_f9f13abea01fb57d94e1acf3" 
+      Payjp::Charge.create(
+        amount: @item.price,  # 商品の値段
+        card: order_params[:token],    # カードトークン
+        currency: 'jpy'                 # 通貨の種類（日本円）
+      )
        @order_shipping_address.save
        redirect_to root_path
     else
@@ -24,6 +30,6 @@ class OrdersController < ApplicationController
   private
 
   def order_params
-    params.require(:order_shipping_address).permit(:post_cord, :shipping_from_id, :city, :street_address, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+    params.require(:order_shipping_address).permit(:post_cord, :shipping_from_id, :city, :street_address, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
   end
 end
