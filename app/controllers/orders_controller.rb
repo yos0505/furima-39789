ccaa749df552a007b1780a960214d886dad9ada8
yos0505 <yos2209@icloud.com>
2024-01-19 19:53:21 +0900

    コントローラー　秘密鍵を環境変数に置き換え

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -29,7 +29,7 @@ class OrdersController < ApplicationController
   end
 
   def pay_item
-    Payjp.api_key = "sk_test_f9f13abea01fb57d94e1acf3" 
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
     Payjp::Charge.create(
       amount: @item.price,  # 商品の値段
       card: order_params[:token],    # カードトークン
