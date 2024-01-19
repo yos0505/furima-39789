becd2b297b1cf178134b9794b0c51c800b1e6374
yos0505 <yos2209@icloud.com>
2024-01-19 20:11:02 +0900

    JavaScript　公開鍵の読み込み

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,5 +1,6 @@
 const pay = () => {
-  const payjp = Payjp('pk_test_22a5df0a816dbb03f5c7573a')
+  const publicKey = gon.public_key
+  const payjp = Payjp(publicKey) // PAY.JPテスト公開鍵
   const elements = payjp.elements();
   const numberElement = elements.create('cardNumber');
   const expiryElement = elements.create('cardExpiry');
